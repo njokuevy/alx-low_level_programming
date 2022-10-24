9d67c9d7183fb7c36b6e05d12fe7550fea3ae728
Njoku Evans <evynjoku@gmail.com>
2022-10-25 00:09:46 +0100

    ALX Task xX

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* puts2 - prints every other character of a string
+* starting with first character, followed by /n
+*
+* @str: string to be printed
+*/
+void puts2(char *str)
+{
+	int len;
+	char l;
+
+	for (len = 0; *(str + len) != 0; len++)
+	{
+		if (len % 2 == 0)
+		{
+			l = *(str + len);
+			_putchar(l);
+		}
+	}
+	_putchar('\n');
+}
